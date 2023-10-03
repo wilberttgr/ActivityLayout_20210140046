bd20bf9658f646c9488a9ae011565739c18d5717
Wilbert Tegar <wilbert.tegar.ft21@mail.umy.ac.id>
2023-10-03 22:34:53 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum1/activity_linear_layout.kt
@@ -0,0 +1,11 @@
+package com.example.praktikum1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class activity_linear_layout : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linear_layout)
+    }
+}
